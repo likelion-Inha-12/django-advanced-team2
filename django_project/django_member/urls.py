d240d086765c0f2c746a520cebc3337a57842458
kihyun <mable0927@gmail.com>
2024-04-07 00:43:33 +0900

    API4,5,6 업데이트

--- django_project/django_member/urls.py
+++ django_project/django_member/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('<int:pk>/', views.read_member),
     #path('update/<int:pk>/', views.update_member),
     #path('delete/<int:pk>/', views.delete_member),
+    path('changePwd/<int:pk>/', views.change_password), #비밀번호 변경
+    path('delete/<int:pk>/', views.delete_member), #회원 삭제
+    path('pressHeart/<int:pk>/', views.press_heart), #하트 누르기
     path('leader/<int:pk>/', views.leader),
     path('list/', views.member_list)
 ]
