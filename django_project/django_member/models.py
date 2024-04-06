6a6388b0e058a44106e21662a4e1b28f8af7b342
dasol <5dasoll37@gmail.com>
2024-04-06 17:09:34 +0900

    기본 세팅 수정

--- django_project/django_member/models.py
+++ django_project/django_member/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # id는 자동으로 생성해 준다해서 적지 않았습니다!
+    email = models.EmailField()
+    is_leader = models.BooleanField(default = False)
+    hearts = models.IntegerField(default = 0)
+
+# 모델 필드는 아래 링크 참고했습니다
+# https://github.com/dkyou7/TIL/blob/master/%ED%8C%8C%EC%9D%B4%EC%8D%AC/Django/5.%20%5BDjango%5D%20Model%20%ED%95%84%EB%93%9C%ED%83%80%EC%9E%85%20%EC%A0%95%EB%A6%AC.md
